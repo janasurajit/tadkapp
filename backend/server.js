70f08f97d12e61cd6313337fe2d6d92332534b23
Surajit Jana <141937951+janasurajit@users.noreply.github.com>
2026-08-13 22:39:14 +0530

    Set default PORT to 8080 in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const app = require("./src/app.js")
 const mysqlpool = require("./src/db/db.js")
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 
 
 mysqlpool.query(`SELECT 1`).then(()=>{
