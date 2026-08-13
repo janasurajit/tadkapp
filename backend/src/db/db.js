7ed0c1e23ddc5616507c3aa35ec8471e99d71c98
Surajit Jana <141937951+janasurajit@users.noreply.github.com>
2026-08-13 22:10:26 +0530

    Change MySQL pool to connection in db.js

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 
 const  urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
 console.log(urlDB)
-const mysqlpool = mysql.createPool(urlDB)
+const mysqlpool = mysql.createConnection(urlDB)
 
 
 module.exports = mysqlpool
