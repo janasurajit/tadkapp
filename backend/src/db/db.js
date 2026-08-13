b4f4ae301b3b3d5ac8858a71580955c7b919be08
Surajit Jana <141937951+janasurajit@users.noreply.github.com>
2026-08-13 22:40:27 +0530

    Refactor MySQL connection to use connection pool

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,7 +3,12 @@ const mysql = require('mysql2/promise');
 
 const  urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
 console.log(urlDB)
-const mysqlpool = mysql.createConnection(urlDB)
+const mysqlpool = mysql.createPool({
+        host: process.env.MYSQLHOST,
+        password: process.env.MYSQLPASSWORD,
+        user: process.env.MYSQLUSER,
+        database: process.env.MYSQLDATABASE
+    })
 
 
 module.exports = mysqlpool
